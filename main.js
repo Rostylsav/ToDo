633801ff90bda9193398ff344086b89604c5097e
Rostyk <pas.ros.bor@gmail.com>
2014-04-04 17:22:42 +0300

    2.2

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 (function(){
+
+    function error(text)
+    {
+        console.log(text);
+    }
     function ifPressEnter()
     {
         if (event.keyCode === 13)
         {
             create({ task: document.getElementById('enterTask').value, status: false}, function(){
-                readAll(function(text){
-                    var colectionOfTask = JSON.parse(text);
-                    showAllTask(colectionOfTask)
-                }, function(text){console.log(text);});
-            }, function(text){console.log(text);});
+                readAll(showAllTask, error);
+            }, error);
         }
     }
 
@@ -20,15 +22,20 @@
             containerOneTask.setAttribute('data-id',id);
             containerOneTask.className="containerOneTask";
 
+        var div = document.createElement('div');
+            div.setAttribute('data-id',id);
+            div.className = 'showValueOfTask';
+
         var checkbox = document.createElement('input');
             checkbox.setAttribute('data-id',id);
             checkbox.className = 'checkbox';
             checkbox.type = 'checkbox';
             checkbox.checked = status;
-
-        var div = document.createElement('div');
-            div.setAttribute('data-id',id);
-            div.className = 'showValueOfTask';
+            if(status == true)
+            {
+                div.className = "showValueOfCheckedTask";
+            }
+            checkbox.addEventListener("click", mark, false);
 
         var button=document.createElement('button');
             button.setAttribute('data-id',id);
@@ -38,24 +45,45 @@
         document.getElementById('enterTask').value=''; 
 
         containerOneTask.appendChild(checkbox);
+
         containerOneTask.appendChild(div);
         containerOneTask.appendChild(button);
         container.appendChild(containerOneTask);
     }
 
-    function showAllTask(colection)
+    function showAllTask()
     {
         document.getElementById('containerShowTask').innerHTML = '';
-        for( var i = 0 ; i < colection.length ; i++)
+        readAll(function(text){
+                    var colectionOfTask = JSON.parse(text);
+                    for( var i = 0 ; i < colectionOfTask.length ; i++)
+                        {
+                            showTask(colectionOfTask[i].task, colectionOfTask[i].status, colectionOfTask[i]._id);
+                        }
+                }, error);
+       
+    }
+    
+    function mark(e)
+    {
+        var status = false;
+        if(e.target.checked)
         {
-            showTask(colection[i].task, colection[i].status, colection[i]._id);
+            status = true;
         }
+
+        readById(function(text){
+            var colection = JSON.parse(text);
+            updataById({task : colection.task, status : status}, function(){
+                showAllTask();
+            },error, e.target.getAttribute('data-id'));
+        }, error, e.target.getAttribute('data-id'));
     }
 
 
     function init()
     {
-
+        showAllTask();
     }
     window.ifPressEnter = ifPressEnter;
     window.init = init;
